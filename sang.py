50e2a315a49020d751149cbac0221e865677e33b
mariyabrahma <129838453+mariyabrahma@users.noreply.github.com>
2023-04-04 14:06:11 +0530

    Add files via upload

---
+++ sang.py
@@ -0,0 +1,10 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+print(len(set1))
+print(type(set1))
+set4 = {"abc", 34, True, 40, "male"}
+#Using the set() constructor to make a set:
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
---
+++ sangeetha.py
@@ -0,0 +1,14 @@
+Dict={}
+print("Empty Dictionary:")
+print(Dict)
+Dict[0]='StMarys'
+Dict[2]='College'
+Dict[3]=1
+print("\nDictionary after adding 3 elements:")
+print(Dict)
+print(Dict[2])
+Dict['Value_set']=2,3,4
+print(Dict)
+Dict[5]={'Nested':{'1':'Happy','2':'place'}}
+print('\nAdding a Nested key:')
+print(Dict)
